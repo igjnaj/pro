ee1246332358e17594bcf0f35be72efe1166c705
Klemplus <155215699+igjnaj@users.noreply.github.com>
2024-10-18 00:18:17 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,5 @@
-getgenv().setmaxfps = 0
+getgenv().capfps = 0 -- less work for cpu, gpu recommend 10 or less
 getgenv().norender = true -- less work for gpu, recommended! sorry for terrible english!
+getgenv().hopafterxmins = 10 -- hop after x min. default 10 (more times you stay, more chance get ban!)
 
-loadstring(game:HttpGet("main"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/igjnaj/pro/refs/heads/main/revmponeclick.lua"))()
