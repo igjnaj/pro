05023324bab7acbda216d851ef344f4ca1981a46
Klemplus <155215699+igjnaj@users.noreply.github.com>
2024-10-18 01:10:02 +0700

    Update revmponeclick.lua

--- revmponeclick.lua
+++ revmponeclick.lua
@@ -942,6 +942,20 @@ local args = {
 	[2] = "Perk1"
 }
 game:GetService("ReplicatedStorage").Events.Data.Equip:FireServer(unpack(args))
+local plr = game:service"Players".LocalPlayer;
+local tween_s = game:service"TweenService";
+local info = TweenInfo.new(1,Enum.EasingStyle.Quad);
+
+function tp(...)
+    local tic_k = tick();
+    local params = {...};
+    local cframe = CFrame.new(params[1],params[2],params[3]);
+    local tween,err = pcall(function()
+    local tween = tween_s:Create(plr.Character["HumanoidRootPart"],info,{CFrame=cframe});
+    tween:Play();
+end)
+   if not tween then return err end
+end
 local function farmpox()
 	while ( true )
 	do
